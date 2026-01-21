6eac09c2bf3dfdf6d6156a83ac53a32f09150b63
aquivecino <nefmaco@hotmail.com>
2026-01-20 21:26:47 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,26 @@
-const CACHE='av-evidencia-v1';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.json'])));
+const CACHE = "av-evidencia-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ])
+    )
+  );
   self.skipWaiting();
 });
-self.addEventListener('activate',e=>{
+
+self.addEventListener("activate", e => {
   e.waitUntil(self.clients.claim());
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
